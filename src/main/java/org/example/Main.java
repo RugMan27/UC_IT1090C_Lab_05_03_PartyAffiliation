ce10a17f16b42077f372d9e555b7e385ef789019
louis smith <smith9lu@mail.uc.edu>
2025-09-02 11:14:22 -0400

    task completed

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter your party affiliation: ");
+        System.out.println("[D]emocrat");
+        System.out.println("[R]epublican");
+        System.out.println("[I]ndependent");
+        System.out.print("> ");
+
+        String affiliation = in.nextLine().toUpperCase();
+        if(affiliation.equals("D")){
+            System.out.println("You get a Democratic Donkey");
+        } else if(affiliation.equals("R")){
+            System.out.println("You get a Republican Elephant");
+        } else if(affiliation.equals("I")){
+            System.out.println("You get a Independent Person");
+        } else {
+            System.out.println("You said your affiliation was: " + affiliation);
+            System.out.println("Run the program again and enter a valid affiliation [D,R,I]!");
+
+        }
+
+    }
+}
